c6571f222893324514f3b29cd4d17419172515ba
nibdo <hello@bloben.com>
2020-12-29 20:55:23 +0100

    update submodules

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import 'bloben-common/index.scss';
 import StoreLayer from 'layers/store-layer';
 import { BrowserRouter } from 'react-router-dom';
 import { createStore, Store } from 'redux';
-import { Provider, useSelector } from 'react-redux';
+import { Provider } from 'react-redux';
 import { persistStore, persistReducer, createTransform } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
 import rootReducer from 'redux/reducers';
-import { Persistor } from 'redux-persist/es/types';
 import { PersistGate } from 'redux-persist/integration/react';
 import LoadingScreen from './bloben-common/components/loading-screen/loading-screen';
 
