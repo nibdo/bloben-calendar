7f87c21c5ecd506d2f26c96204fafb02f418c78b
nibdo <hello@nibdo.com>
2023-01-15 22:53:02 +0100

    Fix electron provider

--- src/layers/electronProvider.tsx
+++ src/layers/electronProvider.tsx
@@ -3,15 +3,15 @@ import { isElectron } from '../env';
 import ElectronApi from '../api/ElectronApi';
 import React, { useEffect, useRef, useState } from 'react';
 
-let interval: any;
 /**
  * Wait for backend to start for electron app
  * @param props
  * @constructor
  */
 const ElectronProvider = (props: any) => {
-  const [isReady, setIsReady] = useState(true);
+  const [isReady, setIsReady] = useState(false);
   const counter = useRef(0);
+  const interval = useRef(undefined);
 
   const checkIfReady = async () => {
     if (import.meta.env.VITE_APP_IS_ELECTRON && isElectron) {
@@ -28,27 +28,29 @@ const ElectronProvider = (props: any) => {
 
     window.localStorage.setItem('apiUrl', apiUrl);
     window.env.apiUrl = apiUrl;
+    counter.current = counter.current + 1;
+
+    if (counter.current > 30) {
+      setIsReady(true);
+      stopInterval();
+    }
 
     try {
       const response = await ElectronApi.ping();
 
       if (response.status === 200) {
         setIsReady(true);
-      }
-    } catch (e: any) {
-      setIsReady(true);
-
-      counter.current = counter.current + 1;
-      if (counter.current > 3) {
-        setIsReady(true);
         stopInterval();
       }
+    } catch (e: any) {
+      // eslint-disable-next-line no-console
+      console.log(e);
     }
   };
 
   const stopInterval = () => {
-    clearInterval(interval);
-    interval = undefined;
+    clearInterval(interval.current);
+    interval.current = undefined;
   };
 
   useEffect(() => {
@@ -58,15 +60,16 @@ const ElectronProvider = (props: any) => {
       return;
     }
 
-    // interval = setInterval(() => {
-    checkIfReady();
-    // }, 500);
+    // @ts-ignore
+    interval.current = setInterval(() => {
+      checkIfReady();
+    }, 500);
 
-    // return () => stopInterval();
+    return () => stopInterval();
   }, []);
 
   useEffect(() => {
-    if (isReady && interval) {
+    if (isReady && interval.current) {
       stopInterval();
     }
   }, [isReady]);
